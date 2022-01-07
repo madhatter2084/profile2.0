bdf3dd47ecf74423c020380b33e6fb9f88cdf138
madhatter <matthew.rikard@gmail.com>
2022-01-07 17:22:43 -0500

    hover effect added to 'tile'

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,25 +1,29 @@
 import "animate.css";
-import "./home.css"
+import "./home.css";
+import Me from "../assets/mattAndAmanda.jpg";
 
-function Home(){
-    return (
-        <>
-        <div className="content header">
-            <div className='header__bg'></div>
-            <section className="aboutMe">
-                <div className="animate__animated animate__zoomIn" id="profilePhoto" alt="My wife and I"></div>
-                <div className=' animate__animated animate__zoomInLeft'>
-                    <h1>Hello, I'm <span className="blue"> MATT</span></h1>
-                    <p className=''>
-                        I build responsive websites
-                    </p>
-                </div>
-            </section>
-        </div>
-
-        
-        </>
-    )
+function Home() {
+  return (
+    <>
+      <div className="content header">
+        <div className="header__bg"></div>
+        <section className="aboutMe">
+          <img
+            src={Me}
+            className="animate__animated animate__zoomIn"
+            id="profilePhoto"
+            alt="My wife and I"
+          />
+          <div className=" animate__animated animate__zoomInLeft">
+            <h1>
+              Hello, I'm <span className="blue"> MATT</span>
+            </h1>
+            <p className="">I build responsive websites</p>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
