0e49047411f3ad6a3842dc5c189185540bb63408
madhatter <matthew.rikard@gmail.com>
2022-01-24 10:39:24 -0500

    fixed js scroll function on navbar to apply appropriate bg color to all nav elements

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -17,12 +17,20 @@ function NavBar() {
   return (
     <>
       <nav className={`nav ${show && "nav__black"}`}>
-        <div className="links animate__animated animate__fadeInDown">
-          <a href="#home" className="name">
+        <div
+          className={`links animate__animated animate__fadeInDown ${
+            show && "nav__black"
+          }`}
+        >
+          <a href="#home" className={`name ${show && "nav__black"}`}>
             Matt Rikard
           </a>
-          <a href="#portfolio">Projects</a>
-          <a href="#resume">Resume</a>
+          <a href="#portfolio" className={`${show && "nav__black"}`}>
+            Projects
+          </a>
+          <a href="#resume" className={`${show && "nav__black"}`}>
+            Resume
+          </a>
         </div>
       </nav>
       <div id="home"></div>
